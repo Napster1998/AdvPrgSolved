bc9c4557f9d5fd6a29dc6d26dc61c3e06d194a5f
Napster1998 <46029472+Napster1998@users.noreply.github.com>
2022-11-18 10:00:50 +0000

    Possibalities

--- costsell.py
+++ costsell.py
@@ -5,9 +5,10 @@ reversed_num1 = 0
 
 num2 = num1
 
-while num2 != 0:                                    1                    12                                  123
-    digit = num2 % 10                               1               2                             3
-    reversed_num1 = reversed_num1 * 10 + digit      321               32                            3
-    num2 //= 10                                     0                                  1                            12
-print(reversed_num1, " ", num1)
+while num2 != 0:
+    digit = num2 % 10
+    print(digit)
+    reversed_num1 = reversed_num1 * 10 + digit
+    num2//= 10
+print(reversed_num1," ", num1)
 
